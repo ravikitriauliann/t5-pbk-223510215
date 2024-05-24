2a9f21561a8c29c2fd65e225e796d50788a28bb0
ravikitriauliann <ravikitriaulian07@gmail.com>
2024-05-25 01:10:57 +0700

    Menambahkan Delete

--- src/App.vue
+++ src/App.vue
@@ -14,37 +14,41 @@
       <q-tabs align="left">
         <q-route-tab to="/home" label="Beranda" />
         <q-route-tab to="/top-seller" label="Top Seller" />
-        <q-route-tab to="/about" label="Tentang Kami" />
+        <q-route-tab
+          to="/about"
+          label="Tentang Kami"
+          @click.prevent="openDrawer"
+        />
       </q-tabs>
     </q-header>
 
     <!-- Drawer -->
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
       <q-scroll-area class="fit">
         <q-list padding class="menu-list">
           <q-item clickable v-ripple>
             <q-item-section avatar>
               <q-icon name="inbox" />
             </q-item-section>
-            <q-item-section> Inbox </q-item-section>
+            <q-item-section class="text-black"> Inbox </q-item-section>
           </q-item>
           <q-item clickable v-ripple>
             <q-item-section avatar>
               <q-icon name="star" />
             </q-item-section>
-            <q-item-section> Star </q-item-section>
+            <q-item-section class="text-black"> Star </q-item-section>
           </q-item>
           <q-item clickable v-ripple>
             <q-item-section avatar>
               <q-icon name="send" />
             </q-item-section>
-            <q-item-section> Send </q-item-section>
+            <q-item-section class="text-black"> Send </q-item-section>
           </q-item>
           <q-item clickable v-ripple>
             <q-item-section avatar>
               <q-icon name="drafts" />
             </q-item-section>
-            <q-item-section> Drafts </q-item-section>
+            <q-item-section class="text-black"> Drafts </q-item-section>
           </q-item>
         </q-list>
       </q-scroll-area>
@@ -98,52 +102,27 @@
 
         <!-- Card Section -->
         <div class="row q-col-gutter-md q-pt-md">
-          <q-card class="col-xs-12 col-sm-6 col-md-4 q-hover-shadow">
-            <q-img
-              src="https://i.pinimg.com/736x/b5/a8/ca/b5a8cab08bcfa25261a58f9cfb8424e8.jpg"
-              class="q-mb-sm"
-            />
+          <q-card
+            v-for="(product, index) in products"
+            :key="index"
+            class="col-xs-12 col-sm-6 col-md-4 q-hover-shadow"
+          >
+            <q-img :src="product.image" class="q-mb-sm" :size="product.size" />
             <q-card-section>
-              <div class="text-subtitle1 text-black">Sneakers 1 kikss</div>
+              <div class="text-subtitle1 text-black">{{ product.name }}</div>
               <div class="text-caption text-black">
-                Description for product 1
+                {{ product.description }}
               </div>
             </q-card-section>
             <q-card-actions align="right">
               <q-btn flat label="Detail" color="primary" />
               <q-btn flat label="Add to Cart" color="primary" />
-            </q-card-actions>
-          </q-card>
-          <q-card class="col-xs-12 col-sm-6 col-md-4 q-hover-shadow">
-            <q-img
-              src="https://i.pinimg.com/736x/28/64/09/2864091c5f37d33f96f5a8f9557079be.jpg"
-              class="q-mb-sm"
-            />
-            <q-card-section>
-              <div class="text-subtitle1 text-black">Sneakers 2 kikss</div>
-              <div class="text-caption text-black">
-                Description for product 2
-              </div>
-            </q-card-section>
-            <q-card-actions align="right">
-              <q-btn flat label="Detail" color="primary" />
-              <q-btn flat label="Add to Cart" color="primary" />
-            </q-card-actions>
-          </q-card>
-          <q-card class="col-xs-12 col-sm-6 col-md-4 q-hover-shadow">
-            <q-img
-              src="https://i.pinimg.com/736x/7a/32/d4/7a32d4947edb504a92791a519e18853b.jpg"
-              class="q-mb-sm"
-            />
-            <q-card-section>
-              <div class="text-subtitle1 text-black">Sneakers 3 kikss</div>
-              <div class="text-caption text-black">
-                Description for product 3
-              </div>
-            </q-card-section>
-            <q-card-actions align="right">
-              <q-btn flat label="Detail" color="primary" />
-              <q-btn flat label="Add to Cart" color="primary" />
+              <q-btn
+                flat
+                label="Delete"
+                color="negative"
+                @click="deleteProduct(index)"
+              />
             </q-card-actions>
           </q-card>
         </div>
@@ -181,15 +160,82 @@ export default {
   setup() {
     const leftDrawerOpen = ref(false);
     const slide = ref("1");
+    const products = ref([
+      {
+        name: "Sneakers 1 kikss",
+        description: "Description for product 1",
+        image: "https://images.journeys.com/images/products/1_813863_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 2 kikss",
+        description: "Description for product 2",
+        image: "https://images.journeys.com/images/products/1_729184_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 3 kikss",
+        description: "Description for product 3",
+        image: "https://images.journeys.com/images/products/1_785521_ZM.jpg",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 4 kikss",
+        description: "Description for product 4",
+        image: "https://images.journeys.com/images/products/1_5122_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 5 kikss",
+        description: "Description for product 5",
+        image: "https://images.journeys.com/images/products/1_5150_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 6 kikss",
+        description: "Description for product 6",
+        image: "https://images.journeys.com/images/products/1_752142_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 7 kikss",
+        description: "Description for product 7",
+        image: "https://images.journeys.com/images/products/1_259094_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 8 kikss",
+        description: "Description for product 8",
+        image: "https://images.journeys.com/images/products/1_810216_ZM.jpg",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+      {
+        name: "Sneakers 9 kikss",
+        description: "Description for product 9",
+        image: "https://images.journeys.com/images/products/1_441156_ZM.JPG",
+        size: "50px", // Atur ukuran gambar di sini
+      },
+    ]);
 
     function toggleLeftDrawer() {
       leftDrawerOpen.value = !leftDrawerOpen.value;
     }
 
+    function deleteProduct(index) {
+      products.value.splice(index, 1);
+    }
+
+    function openDrawer() {
+      leftDrawerOpen.value = true;
+    }
+
     return {
       leftDrawerOpen,
       slide,
+      products,
       toggleLeftDrawer,
+      deleteProduct,
+      openDrawer,
     };
   },
 };
